ae76e25b6c074a38ea77dc7eaa246bb7f21277f8
Jeytery <dimaostapchenko@gmail.com>
2023-02-16 21:57:06 +0200

    git init

---
+++ Sources/NavigationLayer/NavigationLayer.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+protocol Coordinatable: StatelessCompletionable, AnyObject {
+    func start()
+    func add(coordinatable: Coordinatable)
+    func remove(coordinatable: Coordinatable)
+
+    var childCoordinators: [Coordinatable] { get set }
+}
+
+extension Coordinatable {
+    func add(coordinatable: Coordinatable) {
+        guard !childCoordinators.contains(where: {
+            $0 === coordinatable
+        }) else {
+            return
+        }
+        childCoordinators.append(coordinatable)
+    }
+    
+    func remove(coordinatable: Coordinatable) {
+        if childCoordinators.isEmpty { return }
+        if !coordinatable.childCoordinators.isEmpty {
+            coordinatable.childCoordinators
+                .filter {
+                    $0 !== coordinatable
+                }
+                .forEach {
+                    coordinatable.remove(coordinatable: $0)
+                }
+        }
+        for (index, element) in childCoordinators.enumerated() where element === coordinatable {
+            childCoordinators.remove(at: index)
+            break
+        }
+    }
+}
+
+protocol Completionable {
+    associatedtype ReturnData
+    var completion: ((ReturnData) -> Void)? { get set }
+}
+
+protocol StatelessCompletionable {
+    var completion: (() -> Void)? { get set }
+}
